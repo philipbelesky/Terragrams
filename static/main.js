c105c4f9454d991e6d530b94ef46eb31eb739fd2
Philip Belesky <contact@philipbelesky.com>
2020-02-02 15:48:18 +1100

    Add chapter jumps

--- static/main.js
+++ static/main.js
@@ -1,1 +1,14 @@
 // JS here
+
+function chapterJump(timeCode) {
+  const element = document.getElementById("episodeAudio");
+  const times = timeCode.split(':');
+  let seconds = 0;
+  console.log(times)
+  if (times.length > 2) {
+    seconds = (parseInt(times[0]) * 60 * 60) + (parseInt(times[1]) * 60) + parseInt(times[2]);
+  } else {
+    seconds = (parseInt(times[0]) * 60) + parseInt(times[1]);
+  }
+  element.currentTime = seconds
+}
