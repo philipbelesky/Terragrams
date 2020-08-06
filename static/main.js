afae9fd913603d085239b6aa7015b1810ab94486
Philip Belesky <contact@philipbelesky.com>
2020-08-06 17:19:49 +1000

    Setup transcript styles and jumping

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 // JS here
 
+// Skip podcast to the timestamps indicated in the chapter images
 function chapterJump(timeCode) {
   const element = document.getElementById("episodeAudio");
   const times = timeCode.split(':');
@@ -10,5 +11,20 @@ function chapterJump(timeCode) {
   } else {
     seconds = (parseInt(times[0]) * 60) + parseInt(times[1]);
   }
-  element.currentTime = seconds
+  element.currentTime = seconds;
+  console.debug(`Jumping to ${seconds}`);
+}
+
+// Enhance the timestamps in transcripts to become audio links
+let content = document.querySelector(".transcript-content");
+if (content !== null) {
+
+  let allTimeStamps = content.querySelectorAll("code")
+  if (allTimeStamps.length > 0) {
+    allTimeStamps.forEach(function(timeStamp) {
+      var escapedTimeCode = timeStamp.textContent.replace("[", "").replace("]", "");
+      timeStamp.onclick =  function() { chapterJump(escapedTimeCode) };
+    })
+  }
+
 }
